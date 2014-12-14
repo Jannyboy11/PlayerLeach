a71131766b3a5026be6baf45183cfb065699b14a
Jannyboy11 <janstoomlocomotief@hotmail.com>
2014-12-14 17:17:30 +0100

    fix?

--- src/playerlead/CustomChicken.java
+++ src/playerlead/CustomChicken.java
@@ -1,5 +1,22 @@
 package playerlead;
 
-public class CustomChicken extends ChickenEntity {
+import net.minecraft.server.v1_8_R1.Entity;
+
+import org.bukkit.craftbukkit.v1_8_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftEntity;
+import org.bukkit.entity.EntityType;
+
+public class CustomChicken extends CraftEntity {
+
+	public CustomChicken(CraftServer server, Entity entity) {
+		super(server, entity);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public EntityType getType() {
+		return EntityType.CHICKEN;
+				
+	}
 
 }
